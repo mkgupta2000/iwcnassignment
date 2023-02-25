8df3dc3380081eb79e0b22156a8729b2e08f3948
Mayank Gupta <mayankguptamg462@gmail.com>
2023-02-25 23:15:45 +0530

    creating components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { Note } from "./components/Note";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Note />
+      </div>
+    </div>
+  );
 };
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Note = () => {
-  return <div>Note</div>;
+  return (
+    <div className="note-container">
+      <div className="note">
+        <p className="title">This is Note 1</p>
+        <p className="content">Content For Note 1</p>
+      </div>
+      <div className="note">
+        <p className="title">This is Note 2</p>
+        <p className="content">Content For Note 2</p>
+      </div>
+      <div className="note">
+        <p className="title">This is Note 3</p>
+        <p className="content">Content For Note 3</p>
+      </div>
+    </div>
+  );
 };
